408ef3eb1c4380177dd4c511064732d86df1ba83
Victor Presumido <victor.presumido@gmail.com>
2017-11-11 18:55:18 -0200

    Improve User#email validations
    Now email is case insensitive

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to validate_presence_of(:password) }
 
 end
