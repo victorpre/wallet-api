726c4aa7892b59a9416496d797cc8cd6e004ed6f
Victor Presumido <victor.presumido@gmail.com>
2017-11-11 18:41:49 -0200

    Implement User model
    - Install devise gem
    - Validation tests for user

---
+++ db/migrate/20171111201212_add_devise_to_users.rb
@@ -0,0 +1,25 @@
+class AddDeviseToUsers < ActiveRecord::Migration[5.1]
+  def self.up
+    change_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
+
+    end
+
+    add_index :users, :email,                unique: true
+  end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
