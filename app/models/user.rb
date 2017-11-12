23ecbbf9f491e9ac1adac67ace7e7d973af6b3c6
Victor Presumido <victor.presumido@gmail.com>
2017-11-12 04:05:30 -0200

    Migration to add auth_token to User
    AuthToken is generated automatically when user is created
    Two helper methods were developed:
    
    - `generate_auth_token`
    - `expire_auth_token!`

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :trackable, :validatable
   validates :name, presence: true
+  validates :auth_token, uniqueness: true, allow_nil: true
+
+  before_create :generate_auth_token
+
+  def generate_auth_token
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+    self.auth_token
+  end
+
+  def expire_auth_token!
+    self.update_attributes(auth_token: nil)
+  end
 end
