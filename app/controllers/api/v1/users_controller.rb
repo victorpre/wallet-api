90ed8fa65dfecc6d9022b9ee053f1643c873c4a1
Victor Presumido <victor.presumido@gmail.com>
2017-11-11 22:43:37 -0200

    Add Users#create endpoint
    Now the API allows users to be created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,21 @@ module Api
       def show
         render json:  User.find(params[:id])
       end
+
+      def create
+        @user = User.create(user_params)
+        if @user.save
+          render json: @user, status: 201
+        else
+          render json: { errors: @user.errors }, status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
     end
   end
 end
