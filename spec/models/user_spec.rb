aed6abd298e411bab1975e4a572ec1ded88b482d
Victor Presumido <victor.presumido@gmail.com>
2017-11-11 22:45:13 -0200

    Add specs for Users#create endpoint
    Tests for User#crete action

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,4 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to validate_presence_of(:password) }
-
 end
--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -2,16 +2,70 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :request do
 
-  before(:each) do
-    @user = create(:user)
-  end
+  describe 'GET #show' do
 
-  describe "GET #show" do
-    before { get api_v1_user_path(@user.id) }
+    before(:each) do
+      @user = create(:user)
+      get api_v1_user_path(@user.id)
+    end
 
     it 'returns the user' do
       user_email = as_json(response.body)[:email]
       expect(user_email).to eq @user.email
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST #create' do
+
+    context 'when user is succesfully created' do
+      before(:each) do
+        @user_attributes = attributes_for :user
+        post api_v1_users_path, params: { user: @user_attributes }
+      end
+
+      it 'returns the created user' do
+        user_email = as_json(response.body)[:email]
+        expect(user_email).to eq @user_attributes[:email]
+      end
+
+      it 'gives the created status code' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+
+    context 'when user is not created' do
+      before(:each) do
+        @not_user_attributes = { name: 'Annonnymous', password: '1234', password_confirmation: '1234' }
+        post api_v1_users_path, params: { user: @not_user_attributes }
+      end
+
+      it 'returns the errors' do
+        user_errors = as_json(response.body)[:errors]
+        expect(user_errors).not_to be_empty
+      end
+
+      it 'gives the created status code' do
+        expect(response).to have_http_status(422)
+      end
+
+      context 'when user has duplicated email' do
+        before  do
+          @user = create :user
+          @user_attributes = attributes_for :user
+          @user_attributes[:email] = @user[:email]
+        end
+
+        subject { post api_v1_users_path, params: { user: @user_attributes } }
+
+        it 'doesnt create the user' do
+          expect{subject}.not_to change{ User.count }
+        end
+      end
     end
   end
 end
