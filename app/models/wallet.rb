4272bb3b82824f1c1ca5bebda8b9efeea073657c
Victor Presumido <victor.presumido@gmail.com>
2017-11-13 19:24:20 -0200

    Add to Wallet  credit_card limit validation
    - Specs
    - `max_limit` helper method

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,18 @@
 class Wallet < ApplicationRecord
   belongs_to :user
-  validates :limit, presence: true, numericality: { greater_than_or_equal_to: 0  }
+  validates :limit, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  has_many :credit_cards
+
+  validate :limit_less_than_credit_cards
+
+
+  def max_limit
+    credit_cards.sum(:limit)
+  end
+
+  private
+
+  def limit_less_than_credit_cards
+    errors.add(:limit, "can't' be greater than credit cards limits.") if limit > max_limit
+  end
 end
