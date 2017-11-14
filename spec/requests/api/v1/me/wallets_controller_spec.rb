ea4165b4567a9dd639ced3156ad82f52bde4f452
Victor Presumido <victor.presumido@gmail.com>
2017-11-14 10:57:37 -0200

    Refactor routes
    - New namespace `/me` added.
    - Routes renamed by convention

--- spec/requests/api/v1/me/wallets_controller_spec.rb
+++ spec/requests/api/v1/me/wallets_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::WalletsController, type: :request do
+RSpec.describe Api::V1::Me::WalletsController, type: :request do
   before(:each) do
     @user = create(:user, :with_credit_cards)
   end
@@ -9,7 +9,7 @@ RSpec.describe Api::V1::WalletsController, type: :request do
 
   describe 'GET #show' do
     before do
-      get api_v1_wallet_path, headers: valid_headers
+      get api_v1_me_wallet_path, headers: valid_headers
     end
 
     let(:expected_response) { { limit: wallet.limit.to_s,
@@ -21,11 +21,11 @@ RSpec.describe Api::V1::WalletsController, type: :request do
     end
   end
 
-  describe 'PUT #limit' do
+  describe 'PUT #update' do
     context 'when limit is allowed' do
       before do
         params = { wallet: { limit: wallet.max_limit.to_s }}
-        put api_v1_wallet_limit_path(nil, params), headers: valid_headers
+        put api_v1_me_wallet_path(nil, params), headers: valid_headers
       end
 
       let(:expected_response) { { limit: wallet.reload.limit.to_s,
@@ -46,7 +46,7 @@ RSpec.describe Api::V1::WalletsController, type: :request do
     before do
       limit_too_big = wallet.max_limit + 1
       params = { wallet: { limit: limit_too_big.to_s }}
-      put api_v1_wallet_limit_path(nil, params), headers: valid_headers
+      put api_v1_me_wallet_path(nil, params), headers: valid_headers
     end
 
     let(:expected_response) { { limit: [errors_on('wallet', 'limit')[:exceed_max_limit]] } }
