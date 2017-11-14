fd3d5461b6154e40d82cb56f7db45aa8fe6c3a5c
Victor Presumido <victor.presumido@gmail.com>
2017-11-14 07:19:44 -0200

    WIP - PUT wallet#limit endpoint
    Need to update specs

--- app/controllers/api/v1/wallets_controller.rb
+++ app/controllers/api/v1/wallets_controller.rb
@@ -10,6 +10,14 @@ module Api
         render json: wallet_response, status: 201
       end
 
+      def limit
+        if @wallet.update(wallet_params)
+          render json: wallet_response, status: 202
+        else
+          render json: @wallet.errors, status: 406
+        end
+      end
+
       private
 
       def set_wallet
@@ -20,6 +28,10 @@ module Api
         { max_limit: @wallet.max_limit,
           limit:     @wallet.limit,
         }
+      end
+
+      def wallet_params
+        params.require(:wallet).permit(:limit)
       end
     end
   end
--- spec/requests/api/v1/wallets_controller_spec.rb
+++ spec/requests/api/v1/wallets_controller_spec.rb
@@ -6,13 +6,25 @@ RSpec.describe Api::V1::WalletsController, type: :request do
     @wallet = @user.wallet
   end
 
+  let(:expected_response) { { limit: @wallet.limit.to_s,
+                              max_limit: @wallet.max_limit.to_s } }
   describe 'GET #show' do
     before do
       get api_v1_wallet_path, headers: valid_headers
     end
 
-    let(:expected_response) { { limit: @wallet.limit.to_s,
-                                max_limit: @wallet.max_limit.to_s } }
+    it 'returns info about the wallet' do
+      wallet_response = as_json(response.body)
+      expect(wallet_response).to include_json(expected_response)
+    end
+  end
+
+  describe 'PUT #limit' do
+    before do
+      params = { wallet: { limit: @wallet.max_limit.to_s }}
+      put api_v1_wallet_limit_path(nil, params), headers: valid_headers
+    end
+
     it 'returns info about the wallet' do
       wallet_response = as_json(response.body)
       expect(wallet_response).to include_json(expected_response)
