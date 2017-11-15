38ee653ad0d361eeb333169475eb55c14d34827a
Victor Presumido <victor.presumido@gmail.com>
2017-11-15 15:47:07 -0200

    Refactor PaymentService and clean code
    Specs for wrong card_id during payment

--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -1,21 +1,25 @@
 class PaymentService
-  def initialize(card, params)
-    @credit_card = card
+  def initialize(wallet, params)
+    @wallet = wallet
     @amount = params[:amount]
   end
 
-  def make_payment
-    if payment_allowed?
-      @credit_card.balance = @credit_card.balance - amount_to_be_paid
-      @credit_card.save
+  def pay_credit_card_bill(card)
+    return if card.nil?
+    credit_card = card
+    if payment_allowed?(credit_card)
+      credit_card.balance = credit_card.balance - amount_to_be_paid
+      credit_card.save
+    else
+      credit_card.errors.add(:credit_card ,I18n.t(:cannot_pay_more_than_owes))
     end
-    @credit_card
+    credit_card
   end
 
   private
 
-  def payment_allowed?
-    (@credit_card.balance >= amount_to_be_paid )? true : (@credit_card.errors.add(:credit_card ,I18n.t(:cannot_pay_more_than_owes)); false)
+  def payment_allowed?(card)
+    card.balance >= amount_to_be_paid
   end
 
   def amount_to_be_paid
--- spec/requests/api/v1/me/payments_controller_spec.rb
+++ spec/requests/api/v1/me/payments_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Api::V1::Me::PaymentsController, type: :request do
     before do
       @user = create :user
       @credit_card = create :credit_card, limit: 4000, balance: 2000, wallet: @user.wallet
+      @not_my_card = create :credit_card
     end
 
     context 'when card is paid succesfully' do
@@ -31,22 +32,44 @@ RSpec.describe Api::V1::Me::PaymentsController, type: :request do
     end
 
     context 'when card is not paid' do
-      let(:payment_attributes) { { payment: { amount: (@credit_card.balance + 1) } } }
-      subject { post create_payment_api_v1_me_wallet_cards_path(@credit_card.id, payment_attributes), headers: valid_headers }
+      context 'when amount that is being paid is too high' do
+        let(:payment_attributes) { { payment: { amount: (@credit_card.balance + 1) } } }
+        subject { post create_payment_api_v1_me_wallet_cards_path(@credit_card.id, payment_attributes), headers: valid_headers }
 
-      it 'doesnt change credit_card balance' do
-        expect{ subject }.to change{@credit_card.reload.balance}.by(0)
+        it 'doesnt change credit_card balance' do
+          expect{ subject }.to change{@credit_card.reload.balance}.by(0)
+        end
+
+        it 'returns the errors' do
+          subject
+          payment_response = as_json(response.body)
+          expect(payment_response).to include_json({credit_card: [I18n.t('cannot_pay_more_than_owes')]})
+        end
+
+        it 'returns status code 403' do
+          subject
+          expect(response).to have_http_status(403)
+        end
       end
 
-      it 'returns the errors' do
-        subject
-        payment_response = as_json(response.body)
-        expect(payment_response).to include_json({credit_card: [I18n.t('cannot_pay_more_than_owes')]})
-      end
+      context 'when card being paid doesnt belong to user' do
+        let(:payment_attributes) { { payment: { amount: (@not_my_card.balance) } } }
+        subject { post create_payment_api_v1_me_wallet_cards_path(@not_my_card.id, payment_attributes), headers: valid_headers }
 
-      it 'returns status code 403' do
-        subject
-        expect(response).to have_http_status(403)
+        it 'doesnt change credit_card balance' do
+          expect{ subject }.to change{@not_my_card.reload.balance}.by(0)
+        end
+
+        it 'returns the errors' do
+          subject
+          payment_response = as_json(response.body)
+          expect(payment_response).to include_json({credit_card: [I18n.t('cannot_pay_credit_card')]})
+        end
+
+        it 'returns status code 403' do
+          subject
+          expect(response).to have_http_status(403)
+        end
       end
     end
   end
