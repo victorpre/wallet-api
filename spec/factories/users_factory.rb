9c7274bb46fd5e9015bf9830e53f1d8c9115203b
Victor Presumido <victor.presumido@gmail.com>
2017-11-15 01:33:07 -0200

    Refactor factories
    Add person in debt trait

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -12,5 +12,14 @@ FactoryBot.define do
         wallet.credit_cards << create(:credit_card)
       end
     end
+
+    trait :in_debt do
+      after(:create) do |user|
+        wallet = user.wallet
+        4.times do
+          wallet.credit_cards << create(:credit_card, :with_high_balance)
+        end
+      end
+    end
   end
 end
