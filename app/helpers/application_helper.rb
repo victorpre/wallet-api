4184c0b2eb193d98dbc8bdf9dd90356a0b6b47af
Victor Presumido <victor.presumido@gmail.com>
2017-11-16 01:12:08 -0200

    Add Homepage with API Documentation
    Documentation written with markdown

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,26 @@
+module ApplicationHelper
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      space_after_headers: true,
+      tables: true,
+      highlight: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root to: 'home#index'
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show, :create, :update]
